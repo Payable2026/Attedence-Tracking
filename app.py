f0af1ec3f99dcbe19c4b4fb15d625a671453213b
His Payable <his_payable@CHENNAI.ORG>
2026-09-16 10:23:56 +0530

    Your commit message

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ IST = ZoneInfo("Asia/Kolkata")
 
 OFFICE_LAT = 13.056600
 OFFICE_LON = 80.2541370
-DEFAULT_RADIUS = 35
+DEFAULT_RADIUS = 30
 
 # =========================================
 # GOOGLE SHEETS AUTH
